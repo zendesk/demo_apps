2a19abcb5f1a3fd300b04441f58306168134c1a8
Po Chen <pchen@zendesk.com>
2013-12-04 16:45:59 +1100

    most are working except groups

--- basic_user_sample/app.js
+++ basic_user_sample/app.js
@@ -8,10 +8,13 @@
     events: {
       'app.activated': function(){
         var user = this.currentUser();
+        console.log(user.groups());
         this.switchTo('basic_user_info', {
+          "id": user.id(),
           "email": user.email(),
           "name": user.name(),
-          "role": user.role()
+          "role": user.role(),
+          "groups": user.groups()
         });
       }
     }
