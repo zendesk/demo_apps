4662e063c80185dc843aaaefe4a268ff74c788a0
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-06 18:11:12 +1100

    change from wikipedia to apps introducion page.

--- iframe_sample_app_wikipedia/app.js
+++ iframe_sample_app_wikipedia/app.js
@@ -1,15 +1,44 @@
 (function() {
   return {
     events: {
-      'app.activated':'displayWikipedia'
+      'app.activated':'init',
+      'click a.app_view':'handleAppClick',
+      'pane.activated': 'paneActivated'
     },
 
-    displayWikipedia: function() {
-      this.page = {href : "http://en.wikipedia.org/wiki/Zendesk"};
-      this.switchTo('iframePage', {
-    page:            this.page,
-  });
-    }
-  };
+    init: function(data){
+      if (data.firstLoad && this.currentLocation() !== 'top_bar' && this.currentLocation() !== 'nav_bar') {
+        this.resizeIframe({ width : '300px', height : '260px' });
+      }
+    },
 
+    paneActivated: function(data) {
+      if (data.firstLoad) {
+        this.resizeIframe({ width : '1024px', height : '500px' });
+      }
+    },
+
+    resizeIframe: function(dimensions) {
+      this.page = { href : "http://developer.zendesk.com/documentation/apps/introduction.html" };
+      this.switchTo('iframePage', {
+        dimension : dimensions,
+        page : this.page
+      });
+    }
+
+    // handleAppClick: function(){
+    //   this.href = "http://developer.zendesk.com/documentation/apps/introduction.html";
+    //   this.page = {};
+    //   if (this.currentLocation() == 'top_bar' || this.currentLocation() == 'nav_bar'){
+    //     this.page = {
+    //       href : this.href,
+    //       width : '1024px',
+    //       height : '500px'
+    //     }
+    //   }
+    //   this.switchTo('iframePage', {
+    //     page:            this.page,
+    //   });
+    // }
+  };
 }());
