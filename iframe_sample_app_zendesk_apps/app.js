dad6c799f0e51384a46ca25ef46ce8c6632a6cf4
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-07 17:33:32 +1100

    add code comment.

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -2,17 +2,16 @@
   return {
     events: {
       'app.activated':'init',
-      'click a.app_view':'handleAppClick',
       'pane.activated': 'paneActivated'
     },
 
-    init: function(data){
+    init: function(data){//load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
       if (data.firstLoad && this.currentLocation() !== 'top_bar' && this.currentLocation() !== 'nav_bar') {
         this.resizeIframe({ width : '300px', height : '260px' });
       }
     },
 
-    paneActivated: function(data) {
+    paneActivated: function(data) {//load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
         this.resizeIframe({ width : '1024px', height : '500px' });
       }
@@ -25,20 +24,5 @@
         page : this.page
       });
     }
-
-    // handleAppClick: function(){
-    //   this.href = "http://developer.zendesk.com/documentation/apps/introduction.html";
-    //   this.page = {};
-    //   if (this.currentLocation() == 'top_bar' || this.currentLocation() == 'nav_bar'){
-    //     this.page = {
-    //       href : this.href,
-    //       width : '1024px',
-    //       height : '500px'
-    //     }
-    //   }
-    //   this.switchTo('iframePage', {
-    //     page:            this.page,
-    //   });
-    // }
   };
 }());
