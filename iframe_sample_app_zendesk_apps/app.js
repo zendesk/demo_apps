359a780ce4e6d412be5df05d3627a3772aca8957
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-07 17:52:37 +1100

    update app.js to display both support.zendesk.com and the zendesk apps introduction page.

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -7,18 +7,18 @@
 
     init: function(data){//load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
       if (data.firstLoad && this.currentLocation() !== 'top_bar' && this.currentLocation() !== 'nav_bar') {
-        this.resizeIframe({ width : '300px', height : '260px' });
+        this.resizeIframe({ width : '300px', height : '260px' }, { href : "https://support.zendesk.com" });
       }
     },
 
     paneActivated: function(data) {//load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
-        this.resizeIframe({ width : '1024px', height : '500px' });
+        this.resizeIframe({ width : '1024px', height : '500px' }, { href : "http://developer.zendesk.com/documentation/apps/introduction.html" });
       }
     },
 
-    resizeIframe: function(dimensions) {
-      this.page = { href : "http://developer.zendesk.com/documentation/apps/introduction.html" };
+    resizeIframe: function(dimensions, page) {
+      this.page = page;
       this.switchTo('iframePage', {
         dimension : dimensions,
         page : this.page
