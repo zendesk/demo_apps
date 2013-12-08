3705bf1209bbd22a89920d5815d38c001cda3d6f
Po Chen <pchen@zendesk.com>
2013-12-08 18:03:40 +1100

    simplify code/color logic

--- basic_user_sample/app.js
+++ basic_user_sample/app.js
@@ -1,11 +1,5 @@
 (function(){
 
-  var RGB_MOD = 256;
-  var RGB_LIMIT = 255;
-  var GRAY_LIMIT = 230;
-  var WHITEN_FACTOR = 80;
-  var TEXT_COLOR_THRESHOLD = 160;
-
   return {
     defaultState: 'loading',
 
@@ -18,13 +12,10 @@
 
         for (var i = 0; i < groups.length; ++i) {
           var temp = groups[i];
-          var name = temp.name();
-          var color = this.nameToColor(name);
 
           groups[i] = {
             id: temp.id(),
-            name: name,
-            tagColor: "#" + color[0].toString(16) + color[1].toString(16) + color[2].toString(16)
+            name: temp.name()
           };
         }
 
@@ -36,24 +27,6 @@
           "groups": groups
         });
       }
-    },
-
-    nameToColor: function(name){
-      var code = 0;
-
-      for (var j = 0; j < name.length; ++j) {
-        code += name.charCodeAt(j);
-      }
-
-      var R = (code % RGB_MOD) + WHITEN_FACTOR,
-          G = (code * 2 % RGB_MOD) + WHITEN_FACTOR,
-          B = (code * 3 % RGB_MOD) + WHITEN_FACTOR;
-
-      if ((R >= GRAY_LIMIT) && (G >= GRAY_LIMIT) && (B >= GRAY_LIMIT)) {
-        R = G = B = GRAY_LIMIT;
-      }
-
-      return [((R >= RGB_MOD) ? RGB_LIMIT : R), ((G >= RGB_MOD) ? RGB_LIMIT : G), ((B >= RGB_MOD) ? RGB_LIMIT : B)];
     }
   };
 
