71f368c4052014fbf62a492ac9e133d424079db9
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-12 15:57:21 +1100

    This app shows two different ways how modal is invoked, which is either via data attribute or via javascript. hmmm, I should really put this line in a README.md file

--- modal_sample_app/app.js
+++ modal_sample_app/app.js
@@ -2,10 +2,48 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated': 'init',
+      'hide #myModal': 'onHide',
+      'shown #myModal': 'onShown',
+      'show #myModal': 'onShow',
+      'hidden #myModal': 'onHidden',
+      'click #modalSwitch': 'swithToModalJSExample',
+      'click #toggleModalJS': 'displayModal'
     },
 
-    doSomething: function() {
+    init: function(){
+      this.switchTo('modal');
+    },
+
+    onHide: function(){
+      console.log("hide in Process");
+    },
+
+    onShown: function(){
+      console.log("shown in Process");
+    },
+
+    onShow: function(){
+      console.log("show in Process");
+    },
+
+    onHidden: function(){
+      console.log("hidden in Process");
+    },
+
+    swithToModalJSExample: function(){
+      this.switchTo('init');
+    },
+
+    displayModal: function(){
+      this.switchTo('modalJS',{
+        header: "This is a modal invoked by javascript",
+        body: "playing around with JavaScript :)"
+      });
+      this.$('#myModal').modal({
+    	backdrop: true,
+        keyboard: false,
+      });
     }
   };
 
