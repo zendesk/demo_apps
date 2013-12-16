c5528e60ecb8da09745c5c413df5db92efdc8c96
Po Chen <pchen@zendesk.com>
2013-12-17 10:03:44 +1100

    implemented refresh button

--- external_RESTful_api_no_auth_sample/app.js
+++ external_RESTful_api_no_auth_sample/app.js
@@ -7,6 +7,18 @@
       fetchQuote: {
         url: 'http://www.iheartquotes.com/api/v1/random',
         type: 'GET',
+        /**
+         * GET (actually all methods) parameters should be here
+         * instead of hard coded in the url
+         *
+         * For http://forexample.com/path?format=json&blah=foo
+         * you should do the following:
+         *
+         * data: {
+         *   format: 'json',
+         *   blah: 'foo'
+         * }
+         */
         data: {
           format: 'json'
         }
@@ -16,11 +28,17 @@
     events: {
       'app.activated': 'init',
       'fetchQuote.done': 'showQuote',
-      'fetchQuote.fail': 'fetchFail'
+      'fetchQuote.fail': 'fetchFail',
+      'click #refresh': 'refresh'
     },
 
     init: function() {
       this.requestQuote();
+    },
+
+    refresh: function() {
+      this.$("#refresh").text("Wait...").attr("disabled", true);
+      this.init();
     },
 
     requestQuote: function() {
@@ -28,15 +46,16 @@
     },
 
     showQuote: function(data) {
-      /*
-      * data
-      * .json_class -> String
-      * .tags       -> Array of String
-      * .quote      -> String  (the quote)
-      * .link       -> String  (url)
-      * .source     -> String
-      */
+      /**
+       * data
+       * .json_class -> String
+       * .tags       -> Array of String
+       * .quote      -> String  (the quote)
+       * .link       -> String  (url)
+       * .source     -> String
+       */
       this.switchTo('app', data);
+      this.$("#refresh").text("Get Another").attr("disabled", false);
     },
 
     fetchFail: function() {
