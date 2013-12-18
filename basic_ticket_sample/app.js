b9ec3d2b684846f49a88b24be4cd39229fe1ef87
Roman Sandler <romansandler0@gmail.com>
2013-12-18 16:14:48 +1100

    Added I18n and extra functionality

--- basic_ticket_sample/app.js
+++ basic_ticket_sample/app.js
@@ -6,93 +6,25 @@
     events: {
       'app.activated':'initialize', // this event is run once when the app loads and calls the 'initialize' function
       'ticket.type.changed':'initialize', // API event fired when the ticket type changes (eg a ticket is marked as an Incident or a Question is changed to a Problem)
-      'click .demoapp_button':'buttonClicked',
-      'zd_change #tickettypeset':'newTicketType',
-      'click #apitestbutton':'testButtonClicked',
-      'analyseText.done':'reportAnalysis',
-      'analyseText.fail':'analysisFailed',
-      '*.changed': 'detectedChange'
+      'click .demoapp_tab':'tabClicked', //switching views within the app
+      'zd_change #tickettypeset':'newTicketType', //changing the Zendesk-style menu
+      '*.changed': 'detectedChange' // detects changes in the app's UI
     },
 
     requests: {
-      analyseText: function(text) {
-        return {
-          url: 'http://textalyser.net/index.php?lang=en',
-          type: 'POST',
-          data: text,
-          contentType: false,
-          processData: false,
-          /*url: 'http://www.zombo.com/',
-          type: 'GET'*/
-        };
-      }
     },
 
-    initialize: function(data) { // function called when we load or the ticket type is modified
+    initialize: function(data) 
+    { // function called when we load or the ticket type is modified
       var self = this;
-      var templateData = {
-        type_label:        self.getTypeLabel(),
-        type_is_visible:   self.isTypeVisible(),
-        type_is_required:  self.isTypeRequired(),
-        type_options:      self.getTypeOptions(),
-        status_is_visible: self.getStatusVisible(),
-        status_options:    self.getStatusOptions()
-      };
       if (data.firstLoad) {
-        self.switchTo('main', templateData);
+        self.switchTo('main');
       }
-    },
-
-    /* GETTERS */
-
-    getTypeLabel: function() {
-      return this.ticketFields("type").label();
-    },
-
-    isTypeVisible: function() {
-      return this.ticketFields("type").isVisible() ? 'Yes' : 'No';
-    },
-
-    isTypeDisabled: function() {
-      // To be implemented
-    },
-
-    isTypeRequired: function() {
-      return this.ticketFields("type").isRequired() ? 'Yes' : 'No';
-    },
-
-    getTypeOptions: function() {
-      var options = this.ticketFields("type").options(),
-      labels  = [];
-
-      options.forEach(function(element) {
-        labels.push(element.label());
-      });
-
-      return labels;
-    },
-
-    getStatusVisible: function() {
-      return this.ticketFields("status").isVisible() ? 'Yes' : 'No';
-    },
-
-    getStatusDisabled: function() {
-      // To be implemented
-    },
-
-    getStatusOptions: function() {
-      var options = this.ticketFields("status").options(),
-      labels = [];
-
-      options.forEach(function(element) {
-        labels.push(element.label());
-      });
-
-      return labels;
     },
 
     /* UI Events */
-    buttonClicked: function(data) {
+    tabClicked: function(data) 
+    {
       var clicked = data.currentTarget.id;
 
       this.$('.active').toggleClass('active');
@@ -100,34 +32,21 @@
 
       switch (clicked) {
         case 'ticket':
-        this.switchTo('ticket', {
-          ticketType: this.ticket().type(),
-          ticketSubject: this.ticket().subject(),
-          ticketTypes: [{name: 'Question', value: 'question'}, {name: 'Problem', value: 'problem'}, {name: 'Incident', value: 'incident'}, {name: 'Task', value: 'task'}],
-          ticketCCs: this.ticket().collaborators()
-        });
-        this.$('.tickettypeset').data('menu').addObserver('change',this.newTicketType.bind(this));
-        this.$('.ticketcc').zdSearchMenu();
+          this.generateTicketView();
         break;
         case 'user':
-        var the_user = this.currentUser();
-        this.switchTo('user', {
-          uid: the_user.id(),
-          uemail: the_user.email(),
-          uname: the_user.name(),
-          urole: the_user.role(),
-          ugroups: the_user.groups(),
-          uexternid: the_user.externalId()
-        });
+          this.generateUserView();
         break;
         case 'account':
+          this.generateAccountView();
         break;
         default:
         break;
       }
     },
 
-    newTicketType: function(data) {
+    newTicketType: function(data) 
+    {
       var newType = this.$('#'+data.namespace).data('menu').value;
       newType = newType.toLowerCase();
       data.preventDefault();
@@ -137,37 +56,6 @@
       }
     },
 
-    testButtonClicked: function() {
-      var formData = new FormData();
-      formData.append('text_main', this.ticket().description() + "");
-      formData.append('site_to_analyze', 'http://');
-      formData.append('file_to_analyze', null, "");
-      formData.append('min_char', '3');
-      formData.append('special_word', null);
-      formData.append('words_toanalyse', '10');
-      formData.append('count_numbers', '1');
-      formData.append('is_log', '1');
-      formData.append('stoplist_lang', '1');
-      formData.append('stoplist_perso', null);
-      console.dir(formData);
-      this.ajax('analyseText', formData);
-    },
-
-    reportAnalysis: function(data) {
-      console.log('Analysis succeeded');
-      console.dir(data);
-      var searchString = '<tr><td>Readability  (Gunning-Fog Index) : <span class="quote">(6-easy 20-hard)</span></td><td>';
-      var preStart = data.indexOf(searchString);
-      var start = preStart + searchString.length;
-      var end = data.indexOf('<',start);
-      var readability = data.substring(start,end);
-      this.$('.gfIndex')[0].innerHTML = '<br /><strong>Ticket readability</strong> (6 = easiest, 20 = hardest): ' + readability;
-    },
-
-    analysisFailed: function(jqXHR, textStatus, errorThrown) {
-      console.log('Analysis failed');
-      console.dir(jqXHR);
-    },
     detectedChange: function(data)
     {
       if (data.propertyName == 'ticket.type' && data.newValue != this.$('.tickettypeset').data('menu').value)
@@ -175,6 +63,82 @@
         console.log('Changing dropdown in app to: ' + data.newValue);
         this.$('.tickettypeset').data('menu').setValue(data.newValue);
       }
+      if (data.propertyName == 'ticket.collaborators')
+      {
+        console.log('Change to collaborators');
+        this.generateTicketView();
+      }
+    },
+
+    generateTicketView: function()
+    {
+      var the_ticket = this.ticket();
+        var ticketTypes = [{name: this.I18n.t('ticket.types.question'), value: 'question'},
+                           {name: this.I18n.t('ticket.types.problem'), value: 'problem'},
+                           {name: this.I18n.t('ticket.types.incident'), value: 'incident'},
+                           {name: this.I18n.t('ticket.types.task'), value: 'task'}];
+
+        var ccArray = [];
+        _.each(the_ticket.collaborators(), function(collaborator, key, list) {
+          this[key] = {email: "", role: ""};
+          this[key]["email"]=collaborator.email();
+          this[key]["role"]=collaborator.role() ? collaborator.role() : "Not registered";
+        }, ccArray);
+
+        this.switchTo('ticket', {
+          ticketType: the_ticket.type(),
+          ticketSubject: the_ticket.subject(),
+          ticketTypes: ticketTypes,
+          ticketCCs: ccArray
+        });
+
+        this.$('.tickettypeset').data('menu').setValue(the_ticket.type());
+        this.$('.tickettypeset').data('menu').addObserver('change',this.newTicketType.bind(this));
+    },
+
+    generateUserView: function()
+    {
+      var the_user = this.currentUser();
+      var userRoles = [{name: this.I18n.t('agent.roles.end-user'), value: 'end-user'},
+                       {name: this.I18n.t('agent.roles.agent'), value: 'agent'},
+                       {name: this.I18n.t('agent.roles.admin'), value: 'admin'},
+                       {name: this.I18n.t('agent.roles.other'), value: 'other'}];
+
+      var groupArray = [];
+        _.each(the_user.groups(), function(group, key, list) {
+          this[key] = {group: "", id: ""};
+          this[key]["group"]=group.name();
+          this[key]["id"]=group.id();
+        }, groupArray);
+
+        this.switchTo('user', {
+          uid: the_user.id(),
+          uemail: the_user.email(),
+          uname: the_user.name(),
+          urole: the_user.role(),
+          ugroups: groupArray,
+          uexternid: the_user.externalId(),
+          userRoles: userRoles
+        });
+        for (var i =0; i < userRoles.length; i++)
+        {
+          if (the_user.role() == userRoles[i].value || i == userRoles.length-1)
+          {
+            var role_text = this.$('.userroleget').find('li')[i].innerHTML;
+            role_text = '<strong>' + role_text + '</strong>';
+            this.$('.userroleget').find('li')[i].innerHTML = role_text;
+            break;
+          }
+        }
+    },
+
+    generateAccountView: function()
+    {
+      var the_account = this.currentAccount();
+      this.switchTo('account', {
+        id: the_account.id(),
+        subdomain: the_account.subdomain()
+      });
     }
   };
 
