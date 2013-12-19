46bf80a0239200a7bb529febf3c932017f26d698
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-19 17:48:12 +1100

    fix popover resize not working issue, cleanup code, i18n every piece of text, update stylesheet to make the app look better

--- top_bar_sample_app/app.js
+++ top_bar_sample_app/app.js
@@ -1,66 +1,60 @@
 (function() {
 
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-	return {
-		events: {
-			'pane.activated': 'playMusic',
-			'pane.deactivated': 'stopMusic',
-			'notification.merryChristmas': 'christmas',
-			'notification.happyNewYear': 'newYear',
-			'click #show_link': 'showLink'
-		},
+  return {
+    events: {
+      'pane.activated': 'playMusic',
+      'pane.deactivated': 'stopMusic',
+      'notification.christmas': 'christmas',
+      'notification.new_year': 'newYear',
+      'click .show_link': 'showLink'
 
-		startPage: function() {
-			this.switchTo('index');
-			this.popover({width: 400, height: 250});
-			//this.popover();
-		},
+    },
 
-		playMusic: function() {
+    startPage: function() {
+      this.switchTo('index');
+      this.popover({width: 480, height: 320});
+    },
+
+    playMusic: function() {
       if(this.isNotified) {
-        this.audio = this.audio || this.$('#player_audio')[0];
-				console.log(this.audio);
-				console.log(this);
-				this.audio.play();
+        this.$('#player').css('background-image', 'url(' + this.pictureURL + ')');
+        this.audio = this.$('#player_audio')[0];
+        this.audio.play();
+        this.intId = setInterval(function() {
+          var width = getRandomInt(600, 1500);
+          var height = width*2/3;
+          var backgroundSize = width.toString() + "px " + height.toString() + "px";
+          this.$('#player').css('background-size', backgroundSize);
+          this.popover({
+            width: width + 20,
+            height: height + 100
+          });
+        }.bind(this), 100);
+      } else {
+        this.startPage();
+      }
+    },
 
-				this.intId = setInterval(function() {
-					var width = getRandomInt(600, 1500);
-        //this.popover({
-        //    width: width,
-        //    height: width*2/3
-        //});
-				var backgroundSize = width.toString() + "px " + ( width*2/3 ).toString() + "px";
-				this.$('#player').css('background-size', backgroundSize);
-			}.bind(this), 100);
-		} else {
-			this.startPage();
-		}
-		},
-
-		christmas: function() {
-			this.switchTo('player', { christmas: true });
-      this.$('#player').css('background-image', this.assetURL('christmas.jpg'));
-      this.isNotified = true;
-      this.popover({
-        width: 1024,
-        height: 600
-      });
-      this.popover();
-
-      this.playMusic();
+    christmas: function() {
+      this.popover('hide');
+      this.switchTo('player', { christmas: true });
+      this.updatePlayerBackground('christmas.jpg');
     },
 
     newYear: function() {
+      this.popover('hide');
       this.switchTo('player', { newYear: true });
-      this.$('#player').css('background-image', this.assetURL('new_year.jpg'));
+      this.updatePlayerBackground('new_year.jpg');
+    },
+
+    updatePlayerBackground: function(imageName) {
+      this.pictureURL = this.assetURL(imageName);
       this.isNotified = true;
-      this.popover({
-        width: '1024px',
-        height: '600px'
-      });
+      this.popover();
     },
 
     stopMusic: function() {
@@ -75,7 +69,19 @@
     },
 
     showLink: function(e) {
-      var appId = this.$(e.target).data('id');
+      console.log(e);
+      //substring to grab to class name
+      var startIndex = "show_link".length + 1;
+      var eventName = e.toElement.className.substring(startIndex);
+      var uri = e.currentTarget.baseURI.split("/")[2];
+
+      var command = this.I18n.t('command', {
+        app_id: this.id(),
+        event: eventName,
+        uri: uri,
+        email: this.currentUser().email()
+      });
+      this.$('code').text(command);
       this.$('#link').css('display','block');
     }
   };
