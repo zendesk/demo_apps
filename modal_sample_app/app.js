c069cf58ff08c7043eba5fe604f995d9873fc77b
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-20 14:39:30 +1100

    fix code style, remove all ids and use class instead

--- modal_sample_app/app.js
+++ modal_sample_app/app.js
@@ -1,16 +1,17 @@
 (function() {
 
   return {
+
     events: {
       'app.activated':  'modalAttr',
-      'hide #my_modal': 'onHide',
-      'shown #my_modal': 'onShown',
-      'show #my_modal': 'onShow',
-      'hidden #my_modal': 'onHidden',
-      'click #modal_switch_javascript': 'switchToModalJSExample',
-      'click #toggle_modal_javascript': 'displayModal',
-      'click #modal_switch_attribute': 'switchToModalAttrExample',
-      'click #save_button': 'showSavedMessage'
+      'hide .my_modal': 'onHide',
+      'shown .my_modal': 'onShown',
+      'show .my_modal': 'onShow',
+      'hidden .my_modal': 'onHidden',
+      'click .modal_switch_javascript': 'switchToModalJSExample',
+      'click .toggle_modal_javascript': 'displayModal',
+      'click .modal_switch_attribute': 'switchToModalAttrExample',
+      'click .save_button': 'showSavedMessage'
     },
 
     modalAttr: function(hasModalBody) {
@@ -65,7 +66,7 @@
         body: this.I18n.t('modal_body')
       });
       this.isJavascript = true;
-      this.$('#my_modal').modal({
+      this.$('.my_modal').modal({
         backdrop: true,
         keyboard: false
       });
@@ -74,7 +75,7 @@
     showSavedMessage: function() {
       this.modalBody = this.$('.modal-body p').text();
       console.log(this.modalBody);
-      this.$('#my_modal').modal('hide');
+      this.$('.my_modal').modal('hide');
     }
   };
 
