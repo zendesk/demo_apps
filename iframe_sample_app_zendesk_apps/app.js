86c0b6fb3829b8cd26ec8870bb8bda81fd9ed847
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-20 15:46:49 +1100

    fix code style

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -8,20 +8,20 @@
     init: function(data){ //load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
       var sidebarPattern = /_sidebar$/;
       if (data.firstLoad && sidebarPattern.test(this.currentLocation())) {
-        this.resizeIframe({ width : '300px', height : '260px' }, { href : "http://www.wikipedia.com" });
+        this.resizeIframe({ width: '300px', height: '260px' }, { href: "http://www.wikipedia.com" });
       }
     },
 
     paneActivated: function(data) { //load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
-        this.resizeIframe({ width : '1024px', height : '500px' }, { href : "http://developer.zendesk.com/documentation/apps/introduction.html" });
+        this.resizeIframe({ width: '1024px', height: '500px' }, { href: "http://developer.zendesk.com/documentation/apps/introduction.html" });
       }
     },
 
     resizeIframe: function(dimensions, page) {
       this.switchTo('iframePage', {
-        dimension : dimensions,
-        page : page
+        dimension: dimensions,
+        page: page
       });
     }
   };
