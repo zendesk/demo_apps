eba81f8dd3343d43f042be603f765865a34d5315
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-20 14:07:01 +1100

    refine code style

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -1,18 +1,18 @@
 (function() {
   return {
     events: {
-      'app.activated':'init',
+      'app.activated': 'init',
       'pane.activated': 'paneActivated'
     },
 
-    init: function(data){//load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
+    init: function(data){ //load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
       var sidebarPattern = /_sidebar$/;
-      if (data.firstLoad && (sidebarPattern).test(this.currentLocation())) {
+      if (data.firstLoad && sidebarPattern.test(this.currentLocation())) {
         this.resizeIframe({ width : '300px', height : '260px' }, { href : "http://www.wikipedia.com" });
       }
     },
 
-    paneActivated: function(data) {//load content if app is at top_bar and nav_bar
+    paneActivated: function(data) { //load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
         this.resizeIframe({ width : '1024px', height : '500px' }, { href : "http://developer.zendesk.com/documentation/apps/introduction.html" });
       }
