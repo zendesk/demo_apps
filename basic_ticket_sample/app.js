a1f4b7de73da16f191800607373b80515516e8c4
Roman Sandler <romansandler0@gmail.com>
2013-12-20 12:19:24 +1100

    Readability, deleted <br />

--- basic_ticket_sample/app.js
+++ basic_ticket_sample/app.js
@@ -4,13 +4,13 @@
   return { // the entire app goes inside this returh block!
     // listen for API events such as the start of our app, when bits of it get clicked on or when AJAX requests complete
     events: {
-      'app.activated':'initialize', // this event is run once when the app loads and calls the 'initialize' function
-      'ticket.collaborators.changed':'newCCs', // API event fired when the ticket type changes (eg a ticket is marked as an Incident or a Question is changed to a Problem)
-      'click .nav-pills .account':'tabClicked', //switching views within the app
-      'click .nav-pills .user':'tabClicked',
-      'click .nav-pills .ticket':'tabClicked',
-      'zd_ui_change .tickettypeset':'newTicketType', //changing the Zendesk-style menu
-      '*.changed': 'detectedChange' // detects changes in the app's UI
+      'app.activated':                'initialize', // this event is run once when the app loads and calls the 'initialize' function
+      'ticket.collaborators.changed': 'newCCs', // API event fired when the ticket type changes (eg a ticket is marked as an Incident or a Question is changed to a Problem)
+      'click .nav-pills .account':    'tabClicked', //switching views within the app
+      'click .nav-pills .user':       'tabClicked',
+      'click .nav-pills .ticket':     'tabClicked',
+      'zd_ui_change .tickettypeset':  'newTicketType', //changing the Zendesk-style menu
+      '*.changed':                    'detectedChange' // detects changes in the app's UI
     },
 
     // This app doesn't make any AJAX requests but they would go here if it did
@@ -51,7 +51,7 @@
     newTicketType: function(data) {// The ticket type has been changed in our app - better change it in Zendesk
       var newType = this.$('.tickettypeset').zdSelectMenu('value'); // note: you can't use all of jQuery here but selectors are OK
       if (this.ticket().type() != newType) {
-      this.ticket().type(newType);
+        this.ticket().type(newType);
       }
     },
 
@@ -59,9 +59,9 @@
       if (data.propertyName == 'ticket.type' && data.newValue != this.$('.tickettypeset').zdSelectMenu('value'))
       { // The ticket type changed in Zendesk - better change it in our app but not if we initiated the change from the app, that would be an endless loop!
         this.$('.tickettypeset').zdSelectMenu('setValue', data.newValue);
-      }
-      if (data.propertyName == 'ticket.collaborators')
-      { // We could copy the code out of the newCCs function below and put it here
+      } 
+      else if (data.propertyName == 'ticket.collaborators') {
+       // We could copy the code out of the newCCs function below and put it here
       }
     },
 
@@ -71,69 +71,66 @@
     },
 
     generateTicketView: function() {// Draw the 'Ticket' tab
-      var theTicket = this.ticket();
-        var ticketTypes = [{name: this.I18n.t('ticket.types.question'), value: 'question'},
-                           {name: this.I18n.t('ticket.types.problem'), value: 'problem'},
-                           {name: this.I18n.t('ticket.types.incident'), value: 'incident'},
-                           {name: this.I18n.t('ticket.types.task'), value: 'task'}];
+      var theTicket = this.ticket(),
+      ticketTypes = [{name: this.I18n.t('ticket.types.question'), value: 'question'},
+                     {name: this.I18n.t('ticket.types.problem'),  value: 'problem'},
+                     {name: this.I18n.t('ticket.types.incident'), value: 'incident'},
+                     {name: this.I18n.t('ticket.types.task'),     value: 'task'}],
 
-        var ccArray = []; // this array and analogous ones further down are used because Handlebars won't call functions, so we need to pass in properties
-        _.each(theTicket.collaborators(), function(collaborator, key, list) {
-          this[key] = {email: '', role: ''};
-          this[key].email = collaborator.email(); // .email() is the function call we are replacing with the .email property
-          this[key].role = collaborator.role() ? collaborator.role() : 'Not registered';
-        }, ccArray);
+      ccArray = []; // this array and analogous ones further down are used because Handlebars won't call functions, so we need to pass in properties
+      _.each(theTicket.collaborators(), function(collaborator, key, list) {
+        this[key]       = {email: '', role: ''};
+        this[key].email = collaborator.email(); // .email() is the function call we are replacing with the .email property
+        this[key].role  = collaborator.role() ? collaborator.role() : 'Not registered';
+       }, ccArray);
 
-        this.switchTo('ticket', { // render the ticket.hdbs template
-          ticketType: theTicket.type(),
-          ticketSubject: theTicket.subject(),
-          ticketTypes: ticketTypes,
-          ticketCCs: ccArray
-        });
+      this.switchTo('ticket', { // render the ticket.hdbs template
+        ticketType:     theTicket.type(),
+        ticketSubject:  theTicket.subject(),
+        ticketTypes:    ticketTypes,
+        ticketCCs:      ccArray
+      });
 
-        this.$('.tickettypeset').zdSelectMenu('setValue', theTicket.type()); // initialise the Zendesk-stype dropdown to the actual value
+      this.$('.tickettypeset').zdSelectMenu('setValue', theTicket.type()); // initialise the Zendesk-stype dropdown to the actual value
     },
 
     generateUserView: function() {// draw the User tab
-      var theUser = this.currentUser();
-      var userRoles = [{name: this.I18n.t('agent.roles.end-user'), value: 'end-user'},
-                       {name: this.I18n.t('agent.roles.agent'), value: 'agent'},
-                       {name: this.I18n.t('agent.roles.admin'), value: 'admin'},
-                       {name: this.I18n.t('agent.roles.other'), value: 'other'}];
+      var theUser = this.currentUser(),
+      userRoles = [{name: this.I18n.t('agent.roles.end-user'), value: 'end-user'},
+                   {name: this.I18n.t('agent.roles.agent'), value: 'agent'},
+                   {name: this.I18n.t('agent.roles.admin'), value: 'admin'},
+                   {name: this.I18n.t('agent.roles.other'), value: 'other'}],
 
-      var groupArray = []; // this is similar to ccArray in the ticket view - check out the underscore library at http://underscorejs.org
-        _.each(theUser.groups(), function(group, key, list) {
-          this[key] = {group: '', id: ''};
-          this[key].group = group.name();
-          this[key].id = group.id();
-        }, groupArray);
+      groupArray = []; // this is similar to ccArray in the ticket view - check out the underscore library at http://underscorejs.org
+      _.each(theUser.groups(), function(group, key) {
+        this[key]       = {group: '', id: ''};
+        this[key].group = group.name();
+        this[key].id    = group.id();
+      }, groupArray);
 
-        this.switchTo('user', {
-          uid: theUser.id(),
-          uemail: theUser.email(),
-          uname: theUser.name(),
-          urole: theUser.role(),
-          ugroups: groupArray,
-          uexternid: theUser.externalId(),
-          userRoles: userRoles
-        });
-        for (var i = 0; i < userRoles.length; i++) // bold the corrent role
-        {
-          if (theUser.role() == userRoles[i].value || i == userRoles.length-1)
-          {
-            var role_text = this.$('.userroleget').find('li')[i].innerHTML;
-            role_text = '<strong>' + role_text + '</strong>';
-            this.$('.userroleget').find('li')[i].innerHTML = role_text;
-            break;
-          }
+      this.switchTo('user', {
+        uid:        theUser.id(),
+        uemail:     theUser.email(),
+        uname:      theUser.name(),
+        urole:      theUser.role(),
+        ugroups:    groupArray,
+        uexternid:  theUser.externalId(),
+        userRoles:  userRoles
+      });
+      for (var i = 0; i < userRoles.length; i++) {// bold the corrent role
+        if (theUser.role() == userRoles[i].value || i == userRoles.length-1) {
+          var role_text = '<strong>' + this.$('.userroleget').find('li')[i].innerHTML + '</strong>';
+          this.$('.userroleget').find('li')[i].innerHTML = role_text;
+          break;
         }
+      }
     },
 
     generateAccountView: function() {
       var the_account = this.currentAccount();
       this.switchTo('account', {
-        id: the_account.id(),
-        subdomain: the_account.subdomain()
+        id:         the_account.id(),
+        subdomain:  the_account.subdomain()
       });
     }
   };
