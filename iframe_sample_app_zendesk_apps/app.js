d27d2161bdb83c404ba790db4afc371e6668da1b
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-23 16:31:20 +1100

    remove Zendesk apps intro page

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -1,7 +1,6 @@
 (function() {
 
   var SIDE_BAR_HREF = "http://www.wikipedia.com",
-      NAV_BAR_HREF = "http://developer.zendesk.com/documentation/apps/introduction.html",
       SIDE_BAR_REGEX = /_sidebar$/;
 
   return {
@@ -12,20 +11,20 @@
 
     init: function(data){ //load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
       if (data.firstLoad && SIDE_BAR_REGEX.test(this.currentLocation())) {
-        this.showIframe({ width: '300px', height: '260px' }, SIDE_BAR_HREF);
+        this.showIframe({ width: '300px', height: '260px' });
       }
     },
 
     paneActivated: function(data) { //load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
-        this.showIframe({ width: '1024px', height: '500px' }, NAV_BAR_HREF);
+        this.showIframe({ width: '1024px', height: '500px' });
       }
     },
 
     showIframe: function(dimensions, href) {
       this.switchTo('iframePage', {
         dimension: dimensions,
-        href: href
+        href: SIDE_BAR_HREF
       });
     }
   };
