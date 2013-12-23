ab258b42e5301f088b73647e3069c48005ef27a1
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-23 15:35:32 +1100

    strip down this modal app to be invoked by data attribute only.

--- modal_sample_app/README.md
+++ modal_sample_app/README.md
@@ -2,19 +2,18 @@
 
 # Modal Sample App
 
-This Sample App illustrates the usage of modal specific events, and two ways of invoking a modal.
+This sample app illustrates how to implement a simple modal.
 
 ### The following information is displayed:
 
-* location: ticket sidebar and new ticket sidebar
-* show modal via data attribute
-* show modal via javascript
-* switch between the two mentioned above
-* illustrate usage of modal specific events including: show, shown, hide, hidden.
-* display the message from modal on the sidebar app below the toggle modal button.
+* location: ticket sidebar and new ticket sidebar.
+* show modal via data attribute.
+* illustrate usage of modal specific event - hidden.
+* display a static message in modal.
+* use of i18n
 
 Please submit bug reports to [Zendesk](https://support.zendesk.com/requests/new). Pull requests are welcome.
 
 ### Screenshot(s):
 
-![modal](http://f.cl.ly/items/291W002y0U1f1g3t0L3K/modal.gif)
+![modal]()
--- modal_sample_app/app.js
+++ modal_sample_app/app.js
@@ -1,82 +1,24 @@
-(function() {
+(function () {
 
   return {
 
     events: {
       'app.activated': 'modalAttr',
-      'hide .my_modal': 'onHide',
-      'shown .my_modal': 'onShown',
-      'show .my_modal': 'onShow',
-      'hidden .my_modal': 'onHidden',
-      'click .modal_switch_javascript': 'switchToModalJSExample',
-      'click .toggle_modal_javascript': 'displayModal',
-      'click .modal_switch_attribute': 'switchToModalAttrExample',
-      'click .save_button': 'showSavedMessage'
+      'hidden .my_modal': 'onHidden'
     },
 
-    modalAttr: function(hasModalBody) {
-      if (hasModalBody) {
-        this.switchTo('modal', {
-          header: this.I18n.t('modal_header', { method: 'Data Attribute' }),
-          body: this.I18n.t('modal_body'),
-          modal_body: this.modalBody
-        });
-      } else {
-        this.switchTo('modal', {
-          header: this.I18n.t('modal_header', { method: 'Data Attribute' }),
-          body: this.I18n.t('modal_body')
-        });
-      }
-      this.isJavascript = false;
-    },
-
-    onHide: function() {
-      console.log("hide in Process");
-    },
-
-    onShown: function() {
-      console.log("shown in Process");
-    },
-
-    onShow: function() {
-      console.log("show in Process");
-    },
-
-    onHidden: function() {
-      console.log("hidden in Process");
-      if (this.isJavascript) {
-        this.switchTo('modal_js', { modal_body: this.modalBody });
-      } else {
-        this.modalAttr(true);
-      }
-    },
-
-    switchToModalJSExample: function() {
-      this.switchTo('modal_js');
-      this.isJavascript = true;
-    },
-
-    switchToModalAttrExample: function() {
-      this.modalAttr(false);
-    },
-
-    displayModal: function() {
+    modalAttr: function () {
       this.switchTo('modal', {
-        header: this.I18n.t('modal_header', { method: 'JavaScript' }),
+        header: this.I18n.t('modal_header', { method: 'Data Attribute' }),
         body: this.I18n.t('modal_body')
       });
-      this.isJavascript = true;
-      this.$('.my_modal').modal({
-        backdrop: true,
-        keyboard: false
-      });
+
     },
 
-    showSavedMessage: function() {
-      this.modalBody = this.$('.modal-body p').text();
-      console.log(this.modalBody);
-      this.$('.my_modal').modal('hide');
+    onHidden: function () {
+      console.log("hidden in Process");
     }
+
   };
 
 }());
