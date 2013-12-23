9521eaac0900134f330f3335ae8716d153daf3bc
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-23 16:04:40 +1100

    refactor code

--- iframe_sample_app_zendesk_apps/app.js
+++ iframe_sample_app_zendesk_apps/app.js
@@ -1,4 +1,9 @@
 (function() {
+
+  var SIDE_BAR_HREF = "http://www.wikipedia.com",
+      NAV_BAR_HREF = "http://developer.zendesk.com/documentation/apps/introduction.html",
+      SIDE_BAR_REGEX = /_sidebar$/;
+
   return {
     events: {
       'app.activated': 'init',
@@ -6,22 +11,21 @@
     },
 
     init: function(data){ //load content if app is at new_ticket_sidebar, ticket_sidebar and user_sidebar
-      var sidebarPattern = /_sidebar$/;
-      if (data.firstLoad && sidebarPattern.test(this.currentLocation())) {
-        this.resizeIframe({ width: '300px', height: '260px' }, { href: "http://www.wikipedia.com" });
+      if (data.firstLoad && SIDE_BAR_REGEX.test(this.currentLocation())) {
+        this.showIframe({ width: '300px', height: '260px' }, SIDE_BAR_HREF);
       }
     },
 
     paneActivated: function(data) { //load content if app is at top_bar and nav_bar
       if (data.firstLoad) {
-        this.resizeIframe({ width: '1024px', height: '500px' }, { href: "http://developer.zendesk.com/documentation/apps/introduction.html" });
+        this.showIframe({ width: '1024px', height: '500px' }, NAV_BAR_HREF);
       }
     },
 
-    resizeIframe: function(dimensions, page) {
+    showIframe: function(dimensions, href) {
       this.switchTo('iframePage', {
         dimension: dimensions,
-        page: page
+        href: href
       });
     }
   };
