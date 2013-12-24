08aec761130c094aed0b14c6034354ac3a00e1a3
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-24 14:25:30 +1100

    redesign App logic

--- top_bar_sample_app/app.js
+++ top_bar_sample_app/app.js
@@ -2,107 +2,76 @@
 
   'use strict';
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  var MIN = 600,
-      MAX = 1500,
-      WIDTH_BORDER = 20,
-      HEIGHT_BORDER = 100,
-      INTERVAL = 100,
-      RATIO_TOP = 2,
-      RATIO_BOTTOM = 3,
-      INDEX_WIDTH = 400,
-      INDEX_HEIGHT = 320;
+  var EVENT_NAME = "send_message",
+      MEDIUM_WIDTH = 480,
+      MEDIUM_HEIGHT = 300,
+      SMALL_WIDTH = 280,
+      SMALL_HEIGHT = 240,
+      LARGE_WIDTH = 640,
+      LARGE_HEIGHT = 400;
 
   return {
 
     events: {
-
+      'app.activated': 'init',
       'pane.activated': 'paneOnActivated',
       'pane.deactivated': 'paneOnDeactivated',
-      'notification.christmas': 'christmas',
-      'notification.new_year': 'newYear',
-      'click .show_link': 'showLink'
+      'notification.send_message': 'showNoticeBoard',
+      'click .resize_app ul li a': 'resizeAppWindow'
 
     },
 
-    startPage: function() {
-      this.switchTo('index');
-      this.popover({ width: INDEX_WIDTH, height: INDEX_HEIGHT });
+    init: function() {
+      this.isNotified = false; // Set the isNotified tag to false by default.
     },
 
-    paneOnActivated: function() {
-      if (this.isNotified) {
-        this.playMusic();
-      } else {
-        this.startPage();
-      }
-    },
-
-    playMusic: function() {
-      this.$('.player').css('background-image', 'url(' + this.pictureURL + ')');
-      this.audio = this.$('.player_audio')[0];
-      this.audio.play();
-      this.intId = setInterval(function() {
-        var width = getRandomInt(MIN, MAX);
-        var height = width * RATIO_TOP / RATIO_BOTTOM;
-        var backgroundSize = width.toString() + "px " + height.toString() + "px";
-        this.$('.player').css('background-size', backgroundSize);
-        this.popover({
-          width: width + WIDTH_BORDER,
-          height: height + HEIGHT_BORDER
-        });
-      }.bind(this), INTERVAL);
-    },
-
-    christmas: function() {
-      this.popover('hide');
-      this.switchTo('player', { christmas: true });
-      this.updatePlayerBackground('christmas.jpg');
-    },
-
-    newYear: function() {
-      this.popover('hide');
-      this.switchTo('player', { newYear: true });
-      this.updatePlayerBackground('new_year.jpg');
-    },
-
-    updatePlayerBackground: function(imageName) {
-      this.pictureURL = this.assetURL(imageName);
-      this.isNotified = true;
-      this.popover();
-    },
-
-    paneOnDeactivated: function() {
-      if(this.isNotified){
-        this.stopMusic();
-        this.switchTo('index');
-        this.isNotified = false;
-        this.popover({ width: INDEX_WIDTH, height: INDEX_HEIGHT });
-      }
-    },
-
-    stopMusic: function() {
-      clearInterval(this.intId);
-      this.audio.pause();
-    },
-
-    showLink: function(e) {
-      //substring to grab to class name
-      var startIndex = "show_link".length + 1;
-      var eventName = e.toElement.className.substring(startIndex);
+    startPage: function(e) {
+      this.switchTo('instruction');
       var uri = e.currentTarget.baseURI.split("/")[2];
-      //create command
-      var command = this.I18n.t('command', {
+      var command = this.I18n.t('instruction_page.command', {
         app_id: this.id(),
-        event: eventName,
+        event: EVENT_NAME,
         uri: uri,
         email: this.currentUser().email()
       });
       this.$('code').text(command);
-      this.$('.link').css('display','block');
+    },
+
+    paneOnActivated: function(e) {
+      if (!this.isNotified) {
+        this.startPage(e);
+      }
+      this.popover({ width: MEDIUM_WIDTH, height: MEDIUM_HEIGHT });
+    },
+
+    paneOnDeactivated: function() {
+      if (this.isNotified) {
+        this.isNotified = false;
+      }
+    },
+
+    showNoticeBoard: function(data) {
+      this.isNotified = true;
+      this.switchTo('notice_board', {
+        message: data
+      });
+      this.popover();
+    },
+
+    resizeAppWindow: function(e) {
+      switch (e.currentTarget.className) {
+        case 'small':
+          this.popover({ width: SMALL_WIDTH, height: SMALL_HEIGHT });
+          break;
+        case 'medium':
+          this.popover({ width: MEDIUM_WIDTH, height: MEDIUM_HEIGHT });
+          break;
+        case 'largest':
+          this.popover({ width: LARGE_WIDTH, height: LARGE_HEIGHT});
+          break;
+        default:
+          break;
+      }
     }
   };
 
