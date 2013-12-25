180f9857c28210dc82f88770295280df8cbe5fd5
Po Chen <pchen@zendesk.com>
2013-12-26 00:05:06 +1100

    add handlers code

--- save_hook_sample/app.js
+++ save_hook_sample/app.js
@@ -1,12 +1,60 @@
 (function() {
 
   return {
+
+    defaultState: 'showCase',
+
     events: {
-      'app.activated':'doSomething'
+
+      'app.activated': 'init',
+      'ticket.save': 'hookHandler',
+
+      // Switch to different hook handlers
+      'click .simplePass': 'useSimplePass',
+      'click .simpleFail': 'useSimpleFail',
+      'click .stringFail': 'useStringFail'
+
     },
 
-    doSomething: function() {
+    init: function() {
+      this.currentHandler = this.useSimplePass;
+    },
+
+    hookHandler: function() {
+      return this.currentHandler();
+    },
+
+    // Switches
+
+    useSimplePass: function() {
+      this.currentHandler = this.simplePass;
+      console.log("Using simple pass");
+    },
+
+    useSimpleFail: function() {
+      this.currentHandler = this.simpleFail;
+      console.log("Using simple fail");
+    },
+
+    useStringFail: function() {
+      this.currentHandler = this.stringFail;
+      console.log("Using string fail");
+    },
+
+    // Handles
+
+    simplePass: function() {
+      return true;
+    },
+
+    simpleFail: function() {
+      return false;
+    },
+
+    stringFail: function() {
+      return this.I18n.t('fail_string');
     }
+
   };
 
 }());
