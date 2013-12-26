8ebe46e29928c8aaeeab8206ec489a96b9744d1c
Po Chen <pchen@zendesk.com>
2013-12-26 23:40:01 +1100

    add ajax handlers

--- save_hook_sample/app.js
+++ save_hook_sample/app.js
@@ -4,17 +4,28 @@
 
     defaultState: 'showCase',
 
+    requests: {
+      heartQuote: {
+        url: 'http://iheartquotes.com/api/v1/random',
+        data: {
+          format: 'json'
+        }
+      }
+    },
+
     events: {
 
       'app.activated': 'init',
-      'ticket.save': 'hookHandler',
+      'ticket.save':   'hookHandler',
 
       // Switch to different hook handlers
-      'click .simplePass': 'useSimplePass',
-      'click .simpleFail': 'useSimpleFail',
-      'click .stringFail': 'useStringFail',
-      'click .delayedPass': 'useDelayedPass',
-      'click .delayedFail': 'useDelayedFail'
+      'click .simplePass':  'useSimplePass',  // pass
+      'click .simpleFail':  'useSimpleFail',  // fail
+      'click .stringFail':  'useStringFail',  // fail with an error message
+      'click .delayedPass': 'useDelayedPass', // pass 3s after clicking submit
+      'click .delayedFail': 'useDelayedFail', // fail 3s after clicking submit
+      'click .ajaxPass':    'useAjaxPass',    // pass after getting a quote through ajax
+      'click .ajaxFail':    'useAjaxFail'     // fail after getting a quote through ajax
 
     },
 
@@ -53,6 +64,16 @@
       console.log('Using delayed fail');
     },
 
+    useAjaxPass: function() {
+      this.currentHandler = this.ajaxPass;
+      console.log('Using ajax pass');
+    },
+
+    useAjaxFail: function() {
+      this.currentHandler = this.ajaxFail;
+      console.log('Using ajax fail');
+    },
+
     // Handles
 
     simplePass: function() {
@@ -80,6 +101,36 @@
         setTimeout(function() {
           fail();
         }, 3000);
+      });
+    },
+
+    ajaxPass: function() {
+      return this.promise(function(done, fail) {
+        this.ajax('heartQuote').then(
+          function(data) {
+            console.log(data.quote);
+            done();
+          },
+          function() {
+            console.log('ajax failed, but ticket.save shall pass');
+            done();
+          }
+        );
+      });
+    },
+
+    ajaxFail: function() {
+      return this.promise(function(done, fail) {
+        this.ajax('heartQuote').then(
+          function(data) {
+            console.log(data.quote);
+            fail();
+          },
+          function() {
+            console.log('ajax failed, ticket.save shall fail anyway');
+            fail();
+          }
+        );
       });
     }
 
