2eb2bc4bb98478b5cd97e6408f267e25b912d69f
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-30 17:33:23 +1100

    insert more settings

--- settings_sample_app/app.js
+++ settings_sample_app/app.js
@@ -1,21 +1,23 @@
 (function() {
 
+  'use strict';
+
+  var SUBDOMAIN_PATTERN = /\/\/([a-zA-Z0-9]*)\./; // Pattern for extracting subdomain
   return {
     events: {
       'pane.activated':'showAppSettings'
     },
 
     showAppSettings: function(e) {
+
       this.switchTo('mainPage', {
-        subdomain: this.setting('subdomain'),
-        message: this.setting('mainBodyMessage'),
-        hidden_message: this.setting('hiddenSetting'),
+        settings: this.settings,
         email: this.currentUser().email(),
         uri: e.currentTarget.baseURI.split('/')[2], // extract the whole domain name url from the current base uri
         installation_id: this.installationId()
       });
 
-      console.log(this);
+      console.log(this.settings);
     }
   };
 
