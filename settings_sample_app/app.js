4e047ef76c368a4a8d23ebb6dbc7d6f59e40438d
Daniel Sun <hjdaniel.sun@gmail.com>
2013-12-31 11:04:53 +1100

    Refactor Code

--- settings_sample_app/app.js
+++ settings_sample_app/app.js
@@ -2,22 +2,24 @@
 
   'use strict';
 
-  var SUBDOMAIN_PATTERN = /\/\/([a-zA-Z0-9]*)\./; // Pattern for extracting subdomain
+  // Pattern for subdomain.zendesk.com extraction.
+  var SUBDOMAIN_PATTERN = /[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+/;
+
   return {
     events: {
-      'pane.activated':'showAppSettings'
+      'pane.activated': 'showAppSettings'
     },
 
     showAppSettings: function(e) {
+      // Run regular expression to extract subdomain url
+      var regexResult = SUBDOMAIN_PATTERN.exec(e.currentTarget.baseURI);
 
       this.switchTo('mainPage', {
         settings: this.settings,
         email: this.currentUser().email(),
-        uri: e.currentTarget.baseURI.split('/')[2], // extract the whole domain name url from the current base uri
+        uri: regexResult[0], // This gets the matched URL
         installation_id: this.installationId()
       });
-
-      console.log(this.settings);
     }
   };
 
