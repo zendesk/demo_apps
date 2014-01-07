e0eb7f0f16210a5371e27c6ab1fe6d36738f0590
Roman Sandler <romansandler0@gmail.com>
2014-01-07 17:42:03 +1100

    Added capability to toggle Options from dropdowns, refactoring, I18n

--- interface_api_sample/app.js
+++ interface_api_sample/app.js
@@ -8,66 +8,132 @@
       'click .nav-pills .showhide': 'initialize',
       'click .nav-pills .newchange': 'detectedChange',
       'click input': 'checkboxClicked',
-      'click .aenable': 'enableClicked',
-      'click .adisable': 'enableClicked'
+      'click .enable': 'enableClicked',
+      'click .collapse': 'collapseClicked'
     },
 
     initialize: function(event) { // function called when we load
-      var fields = this.ticketFields().map(function(field) {
-        return {  fieldname: field.name(),
-                  label:     field.label(),
-                  visible:   field.isVisible() ? 'checked="checked"' : ''};
-      });
+      var options   = null,
+          nooptions = null,
+          fields    = this.ticketFields().map(
+            function(field) {
+
+              try {
+                field.options();
+              }
+              catch(e) {
+                nooptions = field.name();
+              }
+
+              if ( nooptions != field.name() ) { // we have some options to fetch and make into a nice array
+                options = field.options().map(
+                  function(option) {
+                    console.log('option: ' + option.label() + '\nVisible: ' + option.isVisible());
+                    return { label:       option.label(),
+                             value:       option.value(),
+                             enableText:  option.isEnabled() ? this.I18n.t('showhide.disable') : this.I18n.t('showhide.enable'),
+                             oVisible:    option.isVisible()};
+                  }, this);
+              } else {
+                options = null;
+              }
+
+              return {  fieldname: field.name(),
+                        label:     field.label(),
+                        visible:   field.isVisible() ? 'checked=checked' : '',
+                        options:   options, // either null or the options we nested in here
+                        collapse:  options ? '<td><a class="collapse ' + field.name() + '">+</a></td>':'<td></td>' };
+            }, this);
+
       this.switchTo('showhide', {fields: fields});
     },
 
     /* UI Events */
 
-    detectedChange: function(event)
-    {
+    detectedChange: function(event) { // something has triggered our *.changed observer for the ticket!
       var property,
           newvalue;
-      if (event.type == 'click')
-      {
+      if (event.type == 'click') {
         property = null;
         newvalue = null;
-      }
-      else
-      {
+      } else {
         property = event.propertyName;
         newvalue = event.newValue;
       }
       this.switchTo('newchange', {property: property, newvalue: newvalue});
     },
 
-    checkboxClicked: function(event)
-    {
-      var fieldName = event.currentTarget.parentElement.parentElement.className,
-          isVisible = event.currentTarget.checked,
-          field     = this.ticketFields(fieldName);
+    checkboxClicked: function(event) { // one of the checkboxes has been clicked
+      var targetField = this.findMe(event, this),
+      isVisible  = targetField.isVisible();
 
-      if (isVisible)
-      {
-        field.show();
-      }
-      else
-      {
-        field.hide();
+      if (isVisible) {
+        targetField.hide();
+      } else {
+        targetField.show();
       }
     },
 
-    enableClicked: function(event)
-    {
-      var fieldName = event.currentTarget.parentElement.parentElement.className,
-          field     = this.ticketFields(fieldName);
+    enableClicked: function(event) { // An enable or disable button was clicked
+      var targetField = this.findMe(event, this),
+          action = this.findAction(event.currentTarget, this);
 
-      if (event.currentTarget.className == 'aenable')
-      {
-        field.enable();
+      if (action == 'Enable') {
+        targetField.enable();
+      } else {
+        targetField.disable();
       }
-      else
-      {
-        field.disable();
+    },
+
+    collapseClicked: function(event) { // Hide or show the options associated with a dropdown box
+      if (event.currentTarget.innerText == '-') {
+        this.$('tr.option.' + event.currentTarget.classList[1]).hide('slow');
+        event.currentTarget.innerText = '+';
+      } else {
+        this.$('tr.option.' + event.currentTarget.classList[1]).show();
+        event.currentTarget.innerText = '-';
+      }
+    },
+
+    /* Utility Functions */
+
+    findMe: function(event, self) { // Get a 'handle' to the field or option we want
+      var fieldList = event.currentTarget.parentElement.parentElement.classList,
+          optionValue, fieldName;
+
+      if(fieldList.contains('option')) {
+        optionValue = fieldList[0];
+        fieldName   = fieldList[1];
+        return self.ticketFields(fieldName).options([optionValue]);
+      } else {    // Not an option - must be a field
+
+        fieldName = fieldList[0];
+        return self.ticketFields(fieldName);
+      }
+    },
+
+    findAction: function(clickTarget, self) { // figure out what we want to do - enable or disable?
+      switch (clickTarget.tagName) {
+        case 'INPUT':
+          if (clickTarget.type == 'checkbox') { // We could theoretically use the checkbox status to see if we want to enable or disable.
+            return clickTarget.checked;         // Alternatively, we could use a toggle function. In this app we just use ticketFieldOption.isVisible()
+          }
+          break;
+          case 'A':
+            if (clickTarget.innerText == self.I18n.t('showhide.enable')) {
+              if (clickTarget.classList.contains('toggle')) {
+                clickTarget.innerText = self.I18n.t('showhide.disable');
+              }
+              return 'Enable';
+            } else {
+              if (clickTarget.classList.contains('toggle')) {
+                clickTarget.innerText = self.I18n.t('showhide.enable');
+              }
+              return 'Disable';
+            }
+            break;
+        default:
+          break;
       }
     }
   };
