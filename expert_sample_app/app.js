8302de6ab0d0ef55e63878dc9915431f381d2ef6
Daniel Sun <hjdaniel.sun@gmail.com>
2014-01-08 17:40:23 +1100

    Add search query: searching for all open tickets currently assigned to this user

--- expert_sample_app/app.js
+++ expert_sample_app/app.js
@@ -2,28 +2,59 @@
 
   'use strict';
 
-  var PATTERN = /ticket_sidebar$/,
+  var TICKET_URL_PATTERN = /(.+)\.json$/,
     VAL_MIN = 0,
     VAL_MAX = 100;
 
-  function stopFunction(progress) {
-    clearInterval(progress);
-  }
-
   return {
+
+    requests: {
+      search: function(queryString) {
+        return {
+          url: '/api/v2/search.json?query=' + queryString,
+          type: 'GET',
+          dataType: 'json'
+        };
+      }
+    },
+
     events: {
       'app.activated': 'init',
       'ticket.save': 'saveHookHandler',
       'ticket.submit.start': 'ticketSubmitStartHandler',
       'ticket.submit.done': 'ticketSubmitDoneHandler',
-      'click .comment_body_btn': 'showComment'
+      'click .comment_body_btn': 'showComment',
+      'search.done': 'renderTicketLinks'
     },
 
     init: function() {
-      this.switchTo('modal');
+      var query = 'assignee:' + this.currentUser().email() + '+type:ticket+status:open';
+      console.log(query);
+      this.ticketsInfo = [];
+      this.ajax('search', query);
+    },
+
+    renderTicketLinks: function(data) {
+      console.log(data);
+      _.each(data.results, function(ticket) {
+        var regexResult = TICKET_URL_PATTERN.exec(ticket.url);
+        var ticketUrl = regexResult[1]; // This returns the matched url
+        var ticketSubject = ticket.subject;
+        this.ticketsInfo.push({
+          url: ticketUrl,
+          subject: ticketSubject
+        });
+      }.bind(this));
+      this.switchTo('modal', {
+        ticketsInfo: this.ticketsInfo
+      });
+      this.$('.tickets_list_header h5').text(this.I18n.t('total_ticket_assigned_today', {
+        total: data.count
+      }));
     },
 
     ticketSubmitStartHandler: function() {
+
     },
 
     ticketSubmitDoneHandler: function() {
@@ -32,11 +63,8 @@
       this.$('sr-only').text(this.I18n.t('progress_percentage', {
         percentage: percentage
       }));
-
       clearInterval(this.progress);
-
       this.$('.my_modal').modal('hide');
-
     },
 
     showComment: function() {
@@ -44,16 +72,13 @@
     },
 
     saveHookHandler: function() {
-
       this.commentBody = this.comment().text();
       console.log(this.commentBody);
       this.$('modal-footer').addClass('hidden');
-
       this.$('.my_modal').modal({
         backdrop: true,
         keyboard: false
       });
-
       if(this.commentBody === '') {
         return this.promise(function(done, fail) {
           fail();
@@ -62,38 +87,26 @@
           }.bind(this));
       } else {
         return this.promise(function(done, fail) {
-
           this.showSubmitProgressBar();
-
           this.progressBar = this.$('.bar');
-
           this.currentTime = Date.now();
-
           this.valNow = VAL_MIN;
-
           this.progress = setInterval(function() {
-
             this.goProgress(this.valNow);
-
             if(Date.now() - this.currentTime > 5000) {
               done();
             }
-
           }.bind(this), 500);
-
         });
       }
     },
 
     /* Helpers Go Here. */
 
-    goProgress: function(valNow) {
+    goProgress: function(valNow) { // Bootstrap 2.3 progress bar
       this.valNow += VAL_MAX / 10;
-
       var percentage = valNow * 100 / VAL_MAX;
-
       this.progressBar.css('width', percentage + '%');
-
       this.$('.sr-only').text(this.I18n.t('progress_percentage', {
         percentage: percentage
       }));
@@ -114,13 +127,8 @@
       this.renderModalLabel('modal_header_submit');
       this.$('.alert-block').addClass('hidden');
       this.$('.progress').removeClass('hidden');
-
       this.$('button').addClass('hidden');
-
-
     }
-
-
-};
+  };
 
 }());
