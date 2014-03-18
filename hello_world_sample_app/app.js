0abb93a640afe2e35e0c18528837bb6cb187e84a
Maxime Prades <mprades@zendesk.com>
2014-03-19 10:24:02 +1100

    changes

--- hello_world_sample_app/app.js
+++ hello_world_sample_app/app.js
@@ -2,15 +2,14 @@
 
   return {
     events: {
-      'app.activated':    'sayHello'
+      'app.activated' : 'sayHello'
     },
 
     sayHello: function(){
       var currentUser = this.currentUser().name();
-      this.switchTo('hello',
-        {
-          sayUsername: currentUser
-        });
+      this.switchTo('hello', {
+        username: currentUser
+      });
     },
 
   };
